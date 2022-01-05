b4ec34f838b081e5a8a4c65d7071a063c99305f6
Mohdaqib1 <mohdaqib79656@gmail.com>
2022-01-05 14:48:50 +0530

    hello

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "http://localhost:3000"],
+    allow_origins=[ "https://mohdaqib1.github.io/test-dep"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=['*'],
